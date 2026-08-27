ed0f07da6f63338642ce1df1d9b177824359c6e8
amsar <aymericarnoult@gmail.com>
2026-08-27 18:50:22 +0200

    fill triangles with scanline rasterization

--- main.cpp
+++ main.cpp
@@ -27,10 +27,32 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+
 void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
-    line(ax, ay, bx, by, framebuffer, color);
-    line(bx, by, cx, cy, framebuffer, color);
-    line(cx, cy, ax, ay, framebuffer, color);
+    // sort the vertices, a,b,c in ascending y order (bubblesort yay!)
+    if (ay>by) { std::swap(ax, bx); std::swap(ay, by); }
+    if (ay>cy) { std::swap(ax, cx); std::swap(ay, cy); }
+    if (by>cy) { std::swap(bx, cx); std::swap(by, cy); }
+    int total_height = cy-ay;
+
+    if (ay != by) { // if the bottom half is not degenerate
+        int segment_height = by - ay;
+        for (int y=ay; y<=by; y++) { // sweep the horizontal line from ay to by
+            int x1 = ax + ((cx - ax)*(y - ay)) / total_height;
+            int x2 = ax + ((bx - ax)*(y - ay)) / segment_height;
+            for (int x=std::min(x1,x2); x<std::max(x1,x2); x++)  // draw a horizontal line
+                framebuffer.set(x, y, color);
+        }
+    }
+    if (by != cy) { // if the upper half is not degenerate
+        int segment_height = cy - by;
+        for (int y=by; y<=cy; y++) { // sweep the horizontal line from by to cy
+            int x1 = ax + ((cx - ax)*(y - ay)) / total_height;
+            int x2 = bx + ((cx - bx)*(y - by)) / segment_height;
+            for (int x=std::min(x1,x2); x<std::max(x1,x2); x++)  // draw a horizontal line
+                framebuffer.set(x, y, color);
+        }
+    }
 }
 
 int main(int argc, char** argv) {
