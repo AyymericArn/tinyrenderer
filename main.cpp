f413584ead7fdb452c86c01968c001f499e95efa
AyymericArn <aymericarnoult@gmail.com>
2026-09-01 17:12:47 +0200

    z buffer rasterization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <cmath>
+#include <tuple>
+#include "geometry.h"
+#include "model.h"
 #include "tgaimage.h"
 #include <iostream>
+
+constexpr int width  = 800;
+constexpr int height = 800;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -60,13 +66,14 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
 }
 
-void triangleBBoxZ(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer) {
+void triangleBBoxZ(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &zbuffer, TGAImage &framebuffer, TGAColor color) {
     int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
     int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
     int bbmaxx = std::max(std::max(ax, bx), cx);
     int bbmaxy = std::max(std::max(ay, by), cy);
 
     double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+    if (total_area<1) return; // backface culling + discarding triangles that cover less than a pixel
 
 #pragma omp parallel for
     for (int x=bbminx; x<=bbmaxx; x++) {
@@ -78,31 +85,50 @@ void triangleBBoxZ(int ax, int ay, int az, int bx, int by, int bz, int cx, int c
                 continue; // negative barycentric coordinate => the pixel is outside the triangle
             }
             unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
-            std::clog << "alpha: " << alpha << std::endl;
-            std::clog << "beta: " << beta << std::endl;
-            std::clog << "gamma: "<< gamma << std::endl;
-            std::clog << "total: "<< total_area << std::endl;
-            framebuffer.set(x, y, {z});
+            // std::clog << "alpha: " << alpha << std::endl;
+            // std::clog << "beta: " << beta << std::endl;
+            // std::clog << "gamma: "<< gamma << std::endl;
+            // std::clog << "total: "<< total_area << std::endl;
+            if (z <= zbuffer.get(x, y)[0]) continue;
+            zbuffer.set(x, y, {z});
+            framebuffer.set(x, y, color);
         }
     }
 }
 
+std::tuple<int,int,int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
+    return { (v.x + 1.) *  width/2,       // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
+             (v.y + 1.) * height/2,       // we want to shift the vector (x,y) and then scale it to span the entire screen.
+             (v.z + 1.) *   255./2 };
+}
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
-    TGAImage framebuffer(width, height, TGAImage::GRAYSCALE);
+    Model model("./obj/diablo3_pose/diablo3_pose.obj");
+    TGAImage framebuffer(width, height, TGAImage::RGBA);
+    TGAImage     zbuffer(width, height, TGAImage::GRAYSCALE);
 
     int ax = 17, ay =  4, az =  13;
     int bx = 55, by = 39, bz = 128;
     int cx = 23, cy = 59, cz = 255;
 
-    triangleBBoxZ(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
+    // triangleBBoxZ(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer);
 
     // triangleBBox(  7, 45, 35, 100, 45,  60, framebuffer, red);
     // triangleBBox(120, 35, 90,   5, 45, 110, framebuffer, white);
     // triangleBBox(115, 83, 80,  90, 85, 120, framebuffer, green);
 
+    // draw model
+    for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
+        auto [ax, ay, az] = project(model.vert(i, 0));
+        auto [bx, by, bz] = project(model.vert(i, 1));
+        auto [cx, cy, cz] = project(model.vert(i, 2));
+        TGAColor rnd;
+        for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
+        triangleBBoxZ(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer, rnd);
+    }
+
     framebuffer.write_tga_file("framebuffer.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
 
